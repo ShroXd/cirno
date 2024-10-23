080cd0d2cd05b746ff75258486c1125c379206d3
Atriiy <ShroXdF@gmail.com>
2024-10-23 20:16:01 +0800

    feat(log): add tracing as logger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,8 @@
 use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use chrono::Local;
+use tracing::*;
+use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*};
 
 mod actors;
 mod handlers;
@@ -12,13 +15,26 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting backend server...");
+    info!("Initializing tracing subscriber for logging");
+    let file_name = format!("cirno_{}", Local::now().format("%Y-%m-%d"));
+    let file_appender = tracing_appender::rolling::daily("logs", &file_name);
+    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let subscriber = tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(non_blocking_writer))
+        .with(
+            fmt::layer()
+                .with_writer(std::io::stdout)
+                .with_filter(LevelFilter::DEBUG),
+        );
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
+
+    info!("Starting backend server");
     HttpServer::new(|| {
-        println!("Configuring routes...");
         let mut app = App::new().route("/hello", web::get().to(hello));
 
         if !cfg!(debug_assertions) {
-            println!("Configuring static files...");
             app = app.service(Files::new("/", "./web/dist").index_file("index.html"));
         }
 
