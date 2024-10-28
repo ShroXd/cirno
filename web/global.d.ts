c7d6c27314e965f5a886a92c223d6e476e433c56
Atriiy <ShroXdF@gmail.com>
2024-10-28 14:34:33 +0800

    chore: add material-tailwind

---
+++ web/global.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+declare module "react" {
+  interface HTMLAttributes<T> {
+    placeholder?: string;
+    onPointerEnterCapture?: React.PointerEventHandler<T>;
+    onPointerLeaveCapture?: React.PointerEventHandler<T>;
+  }
+}
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@material-tailwind/react";
 import "./App.css";
 
 function App() {
-  return <div className="bg-red-500">Hello world</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <Button>Nice world</Button>
+    </div>
+  );
 }
 
 export default App;
