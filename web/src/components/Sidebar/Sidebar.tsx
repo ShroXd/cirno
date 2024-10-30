e2b03002b4ecdf335963d7aa5e76e40d61554ec2
Atriiy <ShroXdF@gmail.com>
2024-10-30 20:51:38 +0800

    feat(sidebar): impl a basic side bar

--- web/src/components/Sidebar/Sidebar.tsx
+++ web/src/components/Sidebar/Sidebar.tsx
@@ -1,47 +1,54 @@
-import { Cog6ToothIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { createElement, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
-  Card,
-  Typography,
-  List,
-  ListItem,
-  ListItemPrefix,
-  Input,
-} from "@material-tailwind/react";
-import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
+  FolderIcon,
+  Cog6ToothIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/outline";
+import MovieIcon from "/movie.svg";
 
 export const Sidebar = () => {
-  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  const data = [
+    {
+      value: "library",
+      icon: FolderIcon,
+      path: "/",
+    },
+    {
+      value: "settings",
+      icon: Cog6ToothIcon,
+      path: "/settings",
+    },
+    {
+      value: "test",
+      icon: SparklesIcon,
+      path: "/test",
+    },
+  ];
 
   return (
-    <Card className="w-min-72 h-[calc(100vh-2rem)] max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/15">
-      <div className="mb-2 flex items-center gap-4 p-4">
-        <img
-          src="https://docs.material-tailwind.com/img/logo-ct-dark.png"
-          alt="brand"
-          className="h-8 w-8"
-        />
-        <Typography variant="h5" color="blue-gray">
-          Sidebar
-        </Typography>
+    <div className="flex flex-col items-center gap-2 px-2 py-5 border w-20 border-gray-700 rounded-xl bg-gray-900 shadow-xl">
+      <img src={MovieIcon} alt="Movie Icon" className="w-11 h-11" />
+      <div className="flex flex-col items-center justify-center flex-1 gap-2">
+        {data.map(({ value, icon, path }) => (
+          <button
+            className={`px-3 py-3 rounded-xl focus:outline-none hover:border-transparent transition-all ease-in-out duration-300 shadow-inner s ${
+              location.pathname === path ? "bg-gray-800" : "bg-transparent"
+            }`}
+            key={value}
+            onClick={() => navigate(path)}
+          >
+            {createElement(icon, { className: "w-6 h-6 text-gray-50" })}
+          </button>
+        ))}
       </div>
-      <div className="p-2">
-        <Input
-          icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-          label="Search"
-          crossOrigin="anonymous"
-        />
-      </div>
-      <List>
-        <NavLink to="/setting">
-          <ListItem>
-            <ListItemPrefix>
-              <Cog6ToothIcon className="h-5 w-5" />
-            </ListItemPrefix>
-            {t("sidebar.settings")}
-          </ListItem>
-        </NavLink>
-      </List>
-    </Card>
+    </div>
   );
 };
