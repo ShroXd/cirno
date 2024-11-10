ea06efb1cce86d83506b34090f504fada13d0007
Atriiy <ShroXdF@gmail.com>
2024-11-10 16:08:38 +0800

    feat(library): enhance Library component with loading skeletons and improved layout

--- web/src/pages/Library/Library.tsx
+++ web/src/pages/Library/Library.tsx
@@ -1,5 +1,9 @@
+import { ReactNode, useCallback } from 'react'
 import useSWR from 'swr'
+
 import { TVSeriesDTO } from '../../bindings/TVSeriesDTO'
+import { Typography } from '@material-tailwind/react'
+import { NavLink } from 'react-router-dom'
 
 export const Library = () => {
   const { data, error, isLoading } = useSWR<TVSeriesDTO[]>(
@@ -7,19 +11,55 @@ export const Library = () => {
     (url: string) => fetch(url).then(res => res.json())
   )
 
-  if (error) return <div>Error: {error.message}</div>
-  if (isLoading) return <div>Loading...</div>
+  const container = useCallback(
+    (children: ReactNode) => (
+      // TODO: optimize the grid layout for different screen sizes
+      <div className='grid grid-cols-1 gap-4 gap-y-12 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        {children}
+      </div>
+    ),
+    []
+  )
+
+  if (isLoading) {
+    const skeletons = Array.from({ length: 10 }, (_, i) => (
+      <div className='skeleton h-72 w-48 animate-pulse rounded-lg' key={i} />
+    ))
+    return container(skeletons)
+  }
+  if (error) return container(<div>Error: {error.message}</div>)
 
   return (
-    <div>
-      <h1>Library</h1>
-      <ul>
-        {data?.map(series => (
-          <li className='text-2xl text-gray-800' key={series.title}>
-            {series.title}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Typography className='mb-4 mt-2' variant='h4' color='blue-gray'>
+        Recent
+      </Typography>
+      {container(
+        data?.map((serie: TVSeriesDTO) => (
+          <div
+            className='group flex cursor-pointer select-none flex-col overflow-hidden pb-2'
+            key={serie.title}
+          >
+            <NavLink to={`/series/${serie.id}`} state={{ serie: serie }}>
+              <figure className='relative h-72'>
+                <img
+                  className='h-full w-full rounded-xl object-cover object-center'
+                  src={serie.poster_path ?? ''}
+                  alt={serie.title ?? ''}
+                />
+                <figcaption className='absolute bottom-2 left-2/4 flex w-[calc(100%-1rem)] -translate-x-2/4 justify-between rounded-xl border border-white bg-white/75 px-2 py-1 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm transition-transform duration-500 ease-in-out group-hover:-translate-y-2'>
+                  <div>
+                    <Typography variant='paragraph'>{serie.title}</Typography>
+                    <Typography variant='small' color='gray'>
+                      {serie.year}
+                    </Typography>
+                  </div>
+                </figcaption>
+              </figure>
+            </NavLink>
+          </div>
+        ))
+      )}
+    </>
   )
 }
