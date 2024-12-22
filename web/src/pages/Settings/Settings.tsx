c6d991c89552fee3524b1eba6558f4709c303ebe
Atriiy <ShroXdF@gmail.com>
2024-12-22 10:30:43 +0800

    feat(settings): add language selector to Settings page with i18n support for English, Chinese, and Japanese

--- web/src/pages/Settings/Settings.tsx
+++ web/src/pages/Settings/Settings.tsx
@@ -1,3 +1,20 @@
+import { Select, Option } from '@material-tailwind/react'
+import { useTranslation } from 'react-i18next'
+
 export const Settings = () => {
-  return <div>Settings</div>
+  const { t, i18n } = useTranslation()
+
+  return (
+    <div className='ml-6'>
+      <Select
+        label={t('common.languageSelector')}
+        value={i18n.language}
+        onChange={value => i18n.changeLanguage(value)}
+      >
+        <Option value='en-US'>{t('common.language.en')}</Option>
+        <Option value='zh-CN'>{t('common.language.zh-CN')}</Option>
+        <Option value='jp'>{t('common.language.jp')}</Option>
+      </Select>
+    </div>
+  )
 }
