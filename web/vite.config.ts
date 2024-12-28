fbc30b23e99e58122075aa19764a7a814d76391f
Atriiy <48713853+ShroXd@users.noreply.github.com>
2024-12-28 16:18:02 +0800

    refactor(vite.config): reorder imports and adjust visualizer plugin configuration to prevent opening in CI environments

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer({
-      open: true,
+      open: !process.env.CI,
       filename: 'dist/stats.html',
       gzipSize: true,
       brotliSize: true,
