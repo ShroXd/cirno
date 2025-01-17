557210d341e14cf47a73a3427364c841f85c3c00
Atriiy <48713853+ShroXd@users.noreply.github.com>
2025-01-17 12:29:42 +0800

    feat: enhance ESLint configuration by adding jsx-a11y plugin and updating package dependencies; improve accessibility checks in the codebase

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,14 +1,16 @@
 import js from '@eslint/js'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', '**/src/bindings/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', '**/src/bindings/**'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -20,9 +22,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
